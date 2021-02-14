291f39a87e7200a2978a8fbecaf1025a3127ba4a
Abdulhakeem Dahir <somlutions@gmail.com>
2021-02-13 20:33:24 -0800

    updated api routes

--- models/workouts.js
+++ models/workouts.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-	day: Date,
-	execercises: [
+	day: {
+		type: Date,
+		default: Date.now,
+	},
+	exercises: [
 		{
 			type: String,
 			name: String,
@@ -11,9 +14,9 @@ const WorkoutSchema = new Schema({
 			weight: Number,
 			reps: Number,
 			sets: Number,
-			distance: Number
-		}
-	]
+			distance: Number,
+		},
+	],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,9 +3,9 @@ const path = require("path");
 const { Workout } = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-	Workout.find()
-		.then(workouts => {
-			res.json(workouts);
+	Workout.find({})
+		.then(workout => {
+			res.json(workout);
 		})
 		.catch(err => {
 			res.status(500).json(err);
@@ -13,14 +13,34 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.post("/api/workouts", (req, res) => {
+	console.log(req.body);
 	Workout.create(req.body)
-		.then(workout => {})
+		.then(workout => {
+			res.json(workout);
+		})
 		.catch(err => {
 			res.status(500).json(err);
 		});
 });
 
-router.put("/api/workouts/:id", (req, res) => {});
+router.put("/api/workouts/:id", (req, res) => {
+	const newExercise = req.body;
+
+	Workout.findByIdAndUpdate(req.params.id, {
+		$push: {
+			exercises: newExercise,
+        },
+	},
+    {
+        new: true
+    })
+		.then((workout) => {
+			res.json(workout);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
 
 router.get("/api/workouts/range", (req, res) => {});
 
