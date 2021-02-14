2081c7d5730eb5a203b811ca2c1505024c91a2a3
Abdulhakeem Dahir <somlutions@gmail.com>
2021-02-13 17:31:33 -0800

    used express Router

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useNewUrlParser: true,
 });
 
-require("./routes/htmlRoutes.js")(app);
-require("./routes/apiRoutes.js")(app);
+app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
   console.log("App running on port " + PORT);
