626a991010fc467d3d537b54d2183103e76e445b
Abdulhakeem Dahir <somlutions@gmail.com>
2021-02-13 18:57:29 -0800

    corrected schema

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,27 @@
 const router = require("express").Router();
 const path = require("path");
+const { Workout } = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-
-});
-
-router.put("/api/workouts/:id", (req, res) => {
+	Workout.find()
+		.then(workouts => {
+			res.json(workouts);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
 });
 
 router.post("/api/workouts", (req, res) => {
+	Workout.create(req.body)
+		.then(workout => {})
+		.catch(err => {
+			res.status(500).json(err);
+		});
 });
 
-router.get("/api/workouts/range", (req, res) => {
-});
+router.put("/api/workouts/:id", (req, res) => {});
+
+router.get("/api/workouts/range", (req, res) => {});
 
 module.exports = router;
